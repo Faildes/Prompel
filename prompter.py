c30c9c587eb3aa9799abc699eeedfbe4a533111d
Chattiori <127287320+Faildes@users.noreply.github.com>
2024-05-01 14:57:41 +0900

    Update prompter.py

--- prompter.py
+++ prompter.py
@@ -3,7 +3,7 @@ import math
 import re
 import os
 from .block_lora import lbw_lora
-from typing import Callable, Union, Optional
+from typing import Union, Optional, List, Tuple
 
 re_attention = re.compile(r"""
 \\\(|
