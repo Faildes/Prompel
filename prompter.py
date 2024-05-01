d0faf1fdc3e04a1aa2020bece7cdd77d2e367a95
Chattiori <127287320+Faildes@users.noreply.github.com>
2024-05-01 17:08:58 +0900

    Update prompter.py

--- prompter.py
+++ prompter.py
@@ -277,8 +277,6 @@ class CLIPTextCustomEmbedder(object):
         return chunks, token_count
     
     def get_token_ids(self, texts):
-        if type(texts) == str:
-            texts=[texts]
         token_ids_list = self.tokenizer(
             texts,
             truncation=False,
@@ -357,6 +355,8 @@ class CLIPTextCustomEmbedder(object):
         return torch.hstack(zs)
 
     def process_tokens(self, remade_batch_tokens, batch_multipliers):
+        print(tokens.shape)
+        print(tokens)
         tokens = torch.asarray(remade_batch_tokens).to(self.device)
         # print(tokens.shape)
         # print(tokens)
